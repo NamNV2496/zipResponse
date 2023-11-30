c3b1080eae074976bdbc0a8431704bfdf6e1a9d9
namnv2496 <namnv2496@gmail.com>
2023-11-30 22:40:04 +0700

    add file zip json

--- Readme.md
+++ Readme.md
@@ -3,6 +3,15 @@
 
 call this API in browser and save the zip file
 
+
+API get file with data is a list
+
 // curl --location 'http://localhost:8080/api/download-json-list-zip'
+
+
+API get file with data in JSON from 24.9MB -> 3.66MB
+
+// curl --location 'http://localhost:8080/api/zipJson'
+
 
 
---
+++ src/main/java/com/example/zipresponse/controller/ApiController.java
@@ -0,0 +1,56 @@
+package com.example.zipresponse.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/zipJson")
+    public ResponseEntity<byte[]> zipJsonFile() throws IOException {
+
+        // Read JSON file from resources
+        ClassPathResource jsonResource = new ClassPathResource("large-file.json");
+
+
+//        String zipFilePath = "path/to/your/destination/file.zip";
+//        FileOutputStream fos = new FileOutputStream(zipFilePath);
+//        ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+
+        InputStream jsonStream = jsonResource.getInputStream();
+        byte[] jsonData = jsonStream.readAllBytes();
+
+        // Create a ByteArrayOutputStream to hold the zipped content
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ZipOutputStream zipOut = new ZipOutputStream(baos)) {
+            // Add JSON file to the zip
+            ZipEntry entry = new ZipEntry("jsonfile.json");
+            zipOut.putNextEntry(entry);
+            zipOut.write(jsonData);
+            zipOut.closeEntry();
+        }
+
+        // Set up response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", "jsonfile.zip");
+
+        // Return the zipped content as a byte array
+        return ResponseEntity.ok()
+            .headers(headers)
+            .body(baos.toByteArray());
+    }
+}
+
